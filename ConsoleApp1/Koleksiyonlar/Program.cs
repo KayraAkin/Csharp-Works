d9c5fb570dc81b033d24b62b948e6e3b0608536e
KayraAkin <kayrakin11@hotmail.com>
2023-11-14 20:32:52 +0300

    14.11.2023

---
+++ ConsoleApp1/Koleksiyonlar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Kayra", "Fatma", "Batu", "Olgun", "Ostik" };
+
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            Console.WriteLine("---------------------");
+
+            List<string> isimler2 = new List<string> { "Kayra", "Fatma", "Batu", "Olgun", "Ostik" };
+
+        }
+    }
+}
