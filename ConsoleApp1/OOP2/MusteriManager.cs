60bf3d42f5256f5e411295739cb834c3b1d7cbb6
KayraAkin <kayrakin11@hotmail.com>
2023-11-17 18:18:44 +0300

    17.11.2023

---
+++ ConsoleApp1/OOP2/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    // Manager sınıfları iş sınıflarıdır. Businees classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            
+
+        }
+    }
+}
---
+++ ConsoleApp1/OOP2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GercekMusteri musteri1 = new GercekMusteri();
+
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Kayra";
+            musteri1.Soyadi = "Akın";
+            musteri1.TcNO = "1234578910";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "AknCompany";
+            musteri2.VergiNo = "0123456789";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+
+
+        }
+    }
+}
